b976231227d3d578b445e41d44478fe584255ead
Maniya_791 <maniyamehmoodkhan791@gmail.com>
2024-11-25 04:34:28 -0800

    send time update

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 					<div class="row no-gutters slider-text js-fullheight align-items-center justify-content-center">
 						<div class="col-md-12 ftco-animate">
 							<div class="text w-100 mt-5 text-center">
-								<span class="subheading">Taste.it Restaurant</h2></span>
+								<span class="subheading">Taste.it Maniya Restaurant</h2></span>
 								<h1>Cooking Since</h1>
 								<span class="subheading-2">1958</span>
 							</div>
